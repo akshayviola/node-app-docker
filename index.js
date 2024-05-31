9598aa697418478cdb3e7f2af4e821f579f35670
akshayviola <akshayviola201@gmail.com>
2024-05-31 16:30:50 +0530

    modified index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<p style="color: blue;">Hello, World!</p>');
+  res.send('<p style="color: blue;">Hello, World!!</p>');
 });
 
 app.listen(port, () => {
